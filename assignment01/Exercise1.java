7c5bc424242c9c9f0d852e6f23a0a08e4bba7cb7
U-JALASOFT\Miguel Terceros <MiguelTerceros@MTERCEROSC-DH01.jalasoft.local>
2019-10-23 16:52:05 -0400

    assignment1/ tasks2,3

---
+++ assignment01/Exercise1.java
@@ -0,0 +1,8 @@
+package assignment01;
+
+/* public class Exercise1 {
+    final int MAX_VALUE = 999;
+    public static void main(String[] args) {
+        
+    }
+} */
---
+++ assignment01/Exercise3.java
@@ -0,0 +1,21 @@
+package assignment01;
+
+public class Exercise3 {
+    public boolean checkPalindrome(String inputString) {
+        boolean band = false;
+        String compare = new StringBuilder(inputString).reverse().toString();
+
+        if (inputString.equals(compare)) {
+           band = true; 
+        }
+
+        return band;
+    }
+    public static void main(String[] args) {
+        System.out.println("------------- start -------------");
+        System.out.println("aabaa is palindrome? " + new Exercise3().checkPalindrome("aabaa"));
+        System.out.println("abac is palindrome? " + new Exercise3().checkPalindrome("abac"));
+        System.out.println("a is palindrome? " + new Exercise3().checkPalindrome("a"));
+        System.out.println("------------- end -------------");
+    }
+}
